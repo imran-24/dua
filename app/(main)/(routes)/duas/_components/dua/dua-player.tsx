4b48f623550d15c4e3709d356df1ce473ec2c436
Imran <md.imransyam1956@gmail.com>
2025-01-08 00:40:07 +0600

    audio debug 2

--- app/(main)/(routes)/duas/_components/dua/dua-player.tsx
+++ app/(main)/(routes)/duas/_components/dua/dua-player.tsx
@@ -1,31 +1,43 @@
+// "use client";
+
+// import React from 'react';
+// import { AudioPlayer } from 'react-audio-player-component';
+
+// export const DuaPlayer = ({audio}:{audio: string}) => {
+//   return (
+//     <AudioPlayer 
+//       src={audio}
+//       minimal={false}
+//       width={350}
+//       trackHeight={75}
+//       barWidth={1}
+//       gap={1}
+
+//       visualise={true}
+//       backgroundColor="#FFF8DE"
+//       barColor="#C1D0B5"
+//       barPlayedColor="#99A98F"
+
+//       skipDuration={2}
+//       showLoopOption={true}
+//       showVolumeControl={true}
+
+//       // seekBarColor="purple"
+//       // volumeControlColor="blue"
+//       // hideSeekBar={true}
+//       // hideTrackKnobWhenPlaying={true}
+//     />
+//   )
+// }
+
 "use client";
 
-import React from 'react';
-import { AudioPlayer } from 'react-audio-player-component';
+import React from 'react'
 
-export const DuaPlayer = ({audio}:{audio: string}) => {
+const DuaPlayer = () => {
   return (
-    <AudioPlayer 
-      src={audio}
-      minimal={false}
-      width={350}
-      trackHeight={75}
-      barWidth={1}
-      gap={1}
-
-      visualise={true}
-      backgroundColor="#FFF8DE"
-      barColor="#C1D0B5"
-      barPlayedColor="#99A98F"
-
-      skipDuration={2}
-      showLoopOption={true}
-      showVolumeControl={true}
-
-      // seekBarColor="purple"
-      // volumeControlColor="blue"
-      // hideSeekBar={true}
-      // hideTrackKnobWhenPlaying={true}
-    />
+    <div>DuaPlayer</div>
   )
 }
+
+export default DuaPlayer
